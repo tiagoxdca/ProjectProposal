b0c3a5a88fc9712b887fdd8f0ad77273d8c9bb9d
tiago.almeida <tiago.c.almeida@mindera.com>
2025-12-20 11:22:44 +0000

    Initial Commit

---
+++ ProjectProposalTests/ProjectProposalTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectProposalTests.swift
+//  ProjectProposalTests
+//
+//  Created by (Admin) Tiago Cunha Almeida on 20/12/2025.
+//
+
+import Testing
+@testable import ProjectProposal
+
+struct ProjectProposalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

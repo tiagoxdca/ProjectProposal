e51da72adf854030f63d754c90a3829ec0f8f971
tiago.almeida <tiago.c.almeida@mindera.com>
2025-12-20 14:17:05 +0000

    Fix compile error on method makeRequest

--- ProjectProposal/Data/API/DefaultRequestBuilder.swift
+++ ProjectProposal/Data/API/DefaultRequestBuilder.swift
@@ -15,21 +15,28 @@ public struct DefaultRequestBuilder: RequestBuilding {
     }
 
     public func makeRequest(from endpoint: Endpoint) throws -> URLRequest {
-        var components = URLComponents(url: endpoint.baseURL, resolvingAgainstBaseURL: false)
-        let normalizedPath = endpoint.path.hasPrefix("/") ? endpoint.path : "/" + endpoint.path
-        components?.path = (components?.path ?? "") + normalizedPath
-        components?.queryItems = endpoint.queryItems.isEmpty ? nil : endpoint.queryItems
+        guard var components = URLComponents(url: endpoint.baseURL, resolvingAgainstBaseURL: false) else {
+            throw RequestBuilderError.invalidURLComponents
+        }
 
-        guard let url = components?.url else { throw RequestBuilderError.invalidURLComponents }
+        let normalizedPath = endpoint.path.hasPrefix("/") ? endpoint.path : "/" + endpoint.path
+        let basePath = components.path
+        components.path = basePath + normalizedPath
+
+        components.queryItems = endpoint.queryItems.isEmpty ? nil : endpoint.queryItems
+
+        guard let url = components.url else {
+            throw RequestBuilderError.invalidURLComponents
+        }
 
         var request = URLRequest(url: url)
         request.httpMethod = endpoint.method.rawValue
         request.httpBody = endpoint.body
 
-        // headers default + endpoint (endpoint ganha)
         let merged = defaultHeaders.merging(endpoint.headers, uniquingKeysWith: { _, new in new })
         for (k, v) in merged { request.setValue(v, forHTTPHeaderField: k) }
 
         return request
     }
+
 }
